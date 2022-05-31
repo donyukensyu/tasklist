bca271ff0aae6b3d900c8ab73e347d55fbc441f0
masaya.suzuki <MASAYA.SUZUKI3@grp.tmnf.jp>
2022-05-31 15:47:27 +0900

    UpdateServlet.javaを作成

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -40,7 +40,7 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("task", t);
         request.setAttribute("_token", request.getSession().getId());
 
-        request.getSession().setAttribute("taske_id", t.getId());
+        request.getSession().setAttribute("task_id", t.getId());
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
